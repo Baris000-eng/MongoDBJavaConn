5aef086a2b57380a6d2017c2a3e667444a290fc8
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2022-09-15 00:27:40 +0300

    Update MongoDBJavaConnection.java

--- MongoDBJavaConnection.java
+++ MongoDBJavaConnection.java
@@ -63,6 +63,8 @@ public class App {
         Bson deleteFilter = Filters.eq("name","Elon Musk");
         personalCollection.deleteOne(deleteFilter);
 
+        
+        //Delete operation
         Bson multipleDeletes = Filters.eq("country","England");
         personalCollection.deleteMany(multipleDeletes);
 
